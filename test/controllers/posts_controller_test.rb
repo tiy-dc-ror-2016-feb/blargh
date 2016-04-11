dcc88107b0ddb2704fd58650984e03d35205f601
Russell Osborne <russell@burningpony.com>
2016-04-11 12:11:13 -0400

    Tweaks to tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   test "should get index" do
-    @post = posts(:one)
-    binding.pry
     get :index
     assert_response :success
+
+    assert_equal "", assigns(:posts)
   end
 
   test "should get new" do
     get :new
     assert_response :success
   end
-
 end
